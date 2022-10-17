ad514e4bdc8a77fb140bfc00f1dc13fa5f974f0c
Joe Bashe <28634149+digitalengineering@users.noreply.github.com>
2022-10-17 21:00:49 +0200

    Add default headers for initial request

--- deepbnb/spiders/airbnb.py
+++ deepbnb/spiders/airbnb.py
@@ -112,7 +112,22 @@ class AirbnbSpider(scrapy.Spider):
             search_path = self.__query.replace(', ', '--').replace(' ', '-') + '/homes'
             url = self.__explore_search.build_airbnb_url('s/' + search_path)
 
-            yield scrapy.Request(url, self.parse_landing_page, meta={
+            yield scrapy.Request(url, self.parse_landing_page, headers={
+                'accept':                    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-encoding':           'gzip, deflate, br',
+                'accept-language':           'en-US,en;q=0.9',
+                'cache-control':             'no-cache',
+                'pragma':                    'no-cache',
+                'sec-ch-ua':                 '"Not;A=Brand";v="99", "Chromium";v="106"',
+                'sec-ch-ua-mobile':          '?0',
+                'sec-ch-ua-platform':        '"Linux"',
+                'sec-fetch-dest':            'document',
+                'sec-fetch-mode':            'navigate',
+                'sec-fetch-site':            'none',
+                'sec-fetch-user':            '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent':                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+            }, meta={
                 'playwright':              True,
                 'playwright_include_page': True,
                 'playwright_page_methods': [PageMethod('wait_for_selector', '#data-deferred-state', state='hidden')]
